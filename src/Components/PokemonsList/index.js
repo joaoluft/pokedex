de49be7d1b7e879ffccc46022c84d924e3dcb7ee
João Luft <contato.joaoluft@gmail.com>
2024-03-15 20:14:52 -0300

    Feat: Tailwind CSS and Start screen with Trainers

--- src/Components/PokemonsList/index.js
+++ src/Components/PokemonsList/index.js
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react"
-import { getPokemonsData } from "../../utils"
-import { Button } from "./../Button"
-import { InputText } from "./../InputText"
+import { useEffect, useState } from "react";
+import { getPokemonsData } from "../../utils";
+import { Button } from "./../Button";
+import { InputSearch } from "../InputSearch";
 
 export const PokemonsList = ({ language }) => {
     const [pokemons, setPokemons] = useState([]);
     const [filter, setFilter] = useState({
         quantity: 10,
         search: "",
-        type: [],
+        types: [],
         language: language
     });
 
@@ -29,14 +29,16 @@ export const PokemonsList = ({ language }) => {
     }
 
     return (
-        <>
-            <InputText onChange={filterSearchHandler} placeholder="Digite o pokemon à buscar..." />
+        <section>
+            <div className="w-fit">
+                <InputSearch onChange={filterSearchHandler} placeholder="Digite o pokemon à buscar..." />
+            </div>
 
             <div>
                 {pokemons && pokemons.map(pokemon => <div key={pokemon.id}>{pokemon.name[language]}</div>)}
             </div>
 
             <Button onClick={() => changeFilterValue("quantity", filter.quantity + 10)}>Ver mais...</Button>
-        </>
+        </section>
     )
 }
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import pokemons from "./Constants/pokedex.json";
+import trainers from "./Constants/trainers.json";
 
-export const getPokemonsData = async ({quantity, search, type, language}) => {
+export const getPokemonsData = async ({ quantity, search, types, language }) => {
     let filteredList = pokemons;
 
     if (search !== "") {
@@ -9,11 +10,36 @@ export const getPokemonsData = async ({quantity, search, type, language}) => {
         );
     }
 
-    if (type.length > 0) {
+    if (types.length > 0) {
         filteredList = filteredList.filter(pokemon =>
-            pokemon.name[language].toLowerCase().includes(search.toLowerCase())
+            types.every(type => pokemon.type.includes(type))
         );
     }
 
     return filteredList.slice(0, quantity);
+}
+
+export const getTrainersData = async () => {
+
+    let filteredTrainers = {
+        male: [],
+        female: []
+    };
+
+    trainers.forEach(async (trainer) => {
+        const trainerIcon = await import(`./Assets/Images/Trainers/${trainer.icon}`).then((module) => {
+            return module.default;
+        }).catch(err => {
+            console.error("Não foi possível carregar o som do personagem", err);
+        });
+
+        filteredTrainers[trainer.gender].push({
+            id: trainer.id,
+            name: trainer.name,
+            icon: trainerIcon,
+            gender: trainer.gender
+        });
+    });
+
+    return filteredTrainers;
 }
