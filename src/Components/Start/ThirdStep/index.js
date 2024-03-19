ca4b6469f0273198f84408b2916a3b6c09022974
pedroklemtee <pedro.klemt@outlook.com>
2024-03-19 17:03:16 -0300

    Feat: Adicionando imagens aos pokemons por id, e estilizando lista

--- src/Components/Start/ThirdStep/index.js
+++ src/Components/Start/ThirdStep/index.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { Button } from "../../Button";
 import wavingPikachu from "./../../../Assets/Images/Start/waving_pikachu.gif";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export const ThirdStep = ({ userData, setUserData }) => {
-  const [chating, setChating] = useState(1)
+  const [chating, setChating] = useState(1);
 
   const navigate = useNavigate();
 
@@ -12,26 +12,31 @@ export const ThirdStep = ({ userData, setUserData }) => {
     1: `Olá, ${userData.username}! Bem-vindo ao mundo Pokémon, onde aventuras extraordinárias e laços de amizade aguardam por você. Prepare-se para explorar, e se tornar um treinador Pokémon!`,
     2: "Prepare-se para embarcar em uma jornada como treinador Pokémon. Com sua Pokébola em mãos, explore o mundo em busca de criaturas incríveis. Está pronto para se tornar uma lenda?",
     3: "Para capturar um pokémon você deve acessar as informações dele clicando em cima do pokémon desejado, e logo após clicar em capturar para iniciar o processo de captura para sua pokedéx!",
-    4: "Uma dica! Você pode filtrar os pokémons que deseja encontrar com base em tipos, nomes, e quantidade, boa sorte em sua aventura como treinador pokémon!"
-  }
+    4: "Uma dica! Você pode filtrar os pokémons que deseja encontrar com base em tipos, nomes, e quantidade, boa sorte em sua aventura como treinador pokémon!",
+  };
 
   const nextChatHandler = () => {
     const lastQuestion = chating === Object.keys(chats).length;
     if (!lastQuestion) {
-      setChating(prevChat => prevChat + 1);
+      setChating((prevChat) => prevChat + 1);
     } else {
       setUserData((prevData) => ({ ...prevData, tutorial: true }));
-      navigate('/capture');
+      navigate("/capture");
     }
-  }
+  };
 
   return (
     <section className="px-6">
-      <h1 className="text-center text-3xl uppercase text-white font-semibold">Boas-vindas {userData.username}!</h1>
+      <h1 className="text-center text-3xl uppercase text-white font-semibold">
+        Boas-vindas {userData.username}!
+      </h1>
       <div className="w-full flex items-center justify-center">
-        <img className="w-1/2"
+        <img
+          className="w-1/2"
+          draggable="false"
           src={wavingPikachu}
-          alt="Gif animado do pokemon Pikachu Acenando" />
+          alt="Gif animado do pokemon Pikachu Acenando"
+        />
       </div>
       <div className="animate-pulse p-3 bg-gray-50 border-2 border-black rounded-xl shadow-xl">
         <p className="tracking-wide text-gray-700 text-sm">{chats[chating]}</p>
@@ -41,6 +46,6 @@ export const ThirdStep = ({ userData, setUserData }) => {
           {chating === Object.keys(chats).length ? "Finalizar" : "Próximo"}
         </Button>
       </div>
-    </section >
-  )
-}
+    </section>
+  );
+};
--- src/utils.js
+++ src/utils.js
@@ -1,49 +1,80 @@
 import pokemons from "./Constants/pokedex.json";
 import trainers from "./Constants/trainers.json";
 
-export const getPokemonsData = async ({ quantity, search, types, language }) => {
-    let filteredList = pokemons;
+const padStartZero = (number) => {
+  const length = 3;
+  const numberStrignify = number.toString();
 
-    if (search !== "") {
-        filteredList = filteredList.filter(pokemon =>
-            pokemon.name[language].toLowerCase().includes(search.toLowerCase())
-        );
-    }
+  if (numberStrignify.length < length) {
+    return "0".repeat(length - numberStrignify.length) + numberStrignify;
+  } else {
+    return numberStrignify;
+  }
+};
 
-    if (types.length > 0) {
-        filteredList = filteredList.filter(pokemon =>
-            types.every(type => pokemon.type.includes(type))
-        );
-    }
+export const getPokemonsData = async ({
+  quantity,
+  search,
+  types,
+  language,
+}) => {
+  let filteredList = pokemons;
 
-    return filteredList.slice(0, quantity);
-}
+  if (search !== "") {
+    filteredList = filteredList.filter((pokemon) =>
+      pokemon.name[language].toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  if (types.length > 0) {
+    filteredList = filteredList.filter((pokemon) =>
+      types.every((type) => pokemon.type.includes(type))
+    );
+  }
+
+  filteredList = filteredList.slice(0, quantity);
+  filteredList = await Promise.all(
+    filteredList.map(async (pokemon) => {
+      const pokemonIcon = await import(
+        `./Assets/Images/Pokemons/${padStartZero(pokemon.id)}.png`
+      );
+      return { ...pokemon, icon: pokemonIcon.default };
+    })
+  );
+  return filteredList;
+};
 
 export const getTrainersData = async () => {
-    const mountedTrainers = await Promise.all(trainers.map(async (trainer) => {
-        const module = await import(`./Assets/Images/Trainers/${trainer.icon}`);
-        return { ...trainer, icon: module.default };
-    }));
+  const mountedTrainers = await Promise.all(
+    trainers.map(async (trainer) => {
+      const trainerIcon = await import(
+        `./Assets/Images/Trainers/${trainer.icon}`
+      );
+      return { ...trainer, icon: trainerIcon.default };
+    })
+  );
 
-    return mountedTrainers;
-}
+  return mountedTrainers;
+};
 
 export const getTrainerData = async (id) => {
-    const trainer = trainers[id];
-    const module = await import(`./Assets/Images/Trainers/${trainer.icon}`);
-    return { ...trainer, icon: module.default };
-}
+  const trainer = trainers[id];
+  const specificTrainerIcon = await import(
+    `./Assets/Images/Trainers/${trainer.icon}`
+  );
+  return { ...trainer, icon: specificTrainerIcon.default };
+};
 
 export const getPokemonsTypes = async () => {
-    let types = [];
+  let types = [];
 
-    pokemons.forEach(pokemon => {
-        pokemon.type.forEach(type => {
-            if (!types.includes(type)) {
-                types.push(type);
-            }
-        });
+  pokemons.forEach((pokemon) => {
+    pokemon.type.forEach((type) => {
+      if (!types.includes(type)) {
+        types.push(type);
+      }
     });
+  });
 
-    return types;
-}
+  return types;
+};
